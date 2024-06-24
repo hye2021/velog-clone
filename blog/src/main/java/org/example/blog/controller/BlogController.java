5f50b58416ef972bb1e1909aec8c5b108c4d7e9e
hye2021 <hyebeenbyeon@gmail.com>
2024-06-24 15:33:07 +0900

    feat: add dynamic content loading on blog.html
    #3

--- blog/src/main/java/org/example/blog/controller/BlogController.java
+++ blog/src/main/java/org/example/blog/controller/BlogController.java
@@ -1,16 +1,13 @@
 package org.example.blog.controller;
 
 import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
 import org.example.blog.entity.User;
 import org.example.blog.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class BlogController {
@@ -59,18 +56,6 @@ public class BlogController {
         */
 
         return "redirect:/@" + username;
-    }
-
-    @GetMapping("/@{username}")
-    public String userblog(@PathVariable String username,
-                           Model model) {
-        User user = userService.getUsersByUsername(username);
-        if (user == null) {
-            return "redirect:/error";
-        }
-
-        model.addAttribute("user", user);
-        return "blog";
     }
 
     @GetMapping("/userregform")
